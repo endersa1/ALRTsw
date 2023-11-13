c6a08824b7448116be17bbb4849d324eb18848f3
Meredith Doan <mdoan@kennedytech.com>
2023-11-12 21:54:56 -0500

    vib and shock

---
+++ shocker.h/shocker.h
@@ -0,0 +1,31 @@
+
+
+class shocker {
+    public:
+
+    void setupShocker {
+        // shock chg
+        pinMode(8, OUTPUT);
+        // shock dls 
+        pinMode(16, OUTPUT);
+    }
+
+    void charge(bool on) {
+        if(on){
+            digitalWrite(8, HIGH);
+        }
+        else { 
+            digitalWrite(8, LOW);
+        }
+
+    }
+
+    void discharge(bool on) {
+        if(on){
+            digitalWrite(16, HIGH);
+        }
+        else { 
+            digitalWrite(16, LOW);
+        }
+    }
+};
